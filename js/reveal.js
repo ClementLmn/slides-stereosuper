f16d2692039978a832cf0a5feb79112d4412122e
Clément Lemoine <clementlemoine.cl@gmail.com>
2017-08-27 23:11:00 +0200

    Page 2

--- js/reveal.js
+++ js/reveal.js
@@ -39,21 +39,21 @@
 
 			// The "normal" size of the presentation, aspect ratio will be preserved
 			// when the presentation is scaled to fit different resolutions
-			width: "100%",
-			height: "100%",
+			width: 960,
+			height: 700,
 
 			// Factor of the display size that should remain empty around the content
-			margin: 0,
+			margin: 0.1,
 
 			// Bounds for smallest/largest possible scale to apply to content
-			minScale: 1,
-			maxScale: 1,
+			minScale: 0.2,
+			maxScale: 1.5,
 
 			// Display controls in the bottom right corner
 			controls: false,
 
 			// Display a presentation progress bar
-			progress: false,
+			progress: true,
 
 			// Display the page number of the current slide
 			slideNumber: false,
